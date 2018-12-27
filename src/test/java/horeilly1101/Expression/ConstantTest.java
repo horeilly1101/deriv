63f8f5bad3b8d5e85c68013e1bfde2319a7e7d00
Hugh O'Reilly <horeilly1101@gmail.com>
2018-12-27 12:34:56 -0600

    wrote constant unit tests, for completeness

--- src/test/java/horeilly1101/Expression/ConstantTest.java
+++ src/test/java/horeilly1101/Expression/ConstantTest.java
@@ -1,4 +1,22 @@
 package horeilly1101.Expression;
 
+import org.junit.Test;
+
+import static horeilly1101.Expression.Constant.*;
+import static org.junit.Assert.assertEquals;
+
 public class ConstantTest {
+
+  @Test
+  public void derivativeTest() {
+    // constants are tested pretty hard by the other unit tests,
+    // so I'm just going to check their derivatives for the sake
+    // of completeness
+
+    Expression c = constant("c");
+    assertEquals(addID(), c.differentiate("x"));
+
+    Expression one = multID();
+    assertEquals(addID(), one.differentiate("x"));
+  }
 }
