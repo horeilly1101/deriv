f0840a7d44d4510e875062ea6c58f26a062948d6
Hugh O'Reilly <horeilly1101@gmail.com>
2018-12-31 22:40:22 -0600

    updated scanner read method

--- src/main/java/horeilly1101/Parser/Scanner.java
+++ src/main/java/horeilly1101/Parser/Scanner.java
@@ -5,47 +5,38 @@ import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import java_cup.runtime.*;
 
 public class Scanner {
   /**
    * Scans a String into a list of Token objects. We want to filter out
    * the WHITESPACE tokens and break the scanner if a FAIL token pops up.
    */
-//  static Optional<List<Token>> read(String input) {
-//    FlexScanner flex = new FlexScanner(new StringReader(input));
-//    List<Token> tokens = new ArrayList<>();
-//
-//    // iterate until loop breaks
-//    for (;;) {
-//      // default token is a fail
-//      Token current = new Token(SymbolType.FAIL, "exception");
-//      // we need to catch the exception
-//      // (that's never actually going to be thrown)
-//      try {
-//        current = flex.yylex();
-//      } catch (IOException e) {
-//        System.out.println(e);
-//      }
-//
-//      if (current == null) {
-//        break;
-//      }
-//
-//      if (current.name.equals(SymbolType.FAIL)) {
-//        return Optional.empty();
-//      }
-//
-//      // filter out whitespaces
-//      if (!current.name.equals(SymbolType.WHITESPACE)) {
-//        tokens.add(current);
-//      }
-//    }
-//
-//    return Optional.of(tokens);
-//  }
+  static Optional<List<String>> read(String input) {
+    FlexScanner flex = new FlexScanner(new StringReader(input));
+    List<Symbol> tokens = new ArrayList<>();
 
-  enum SymbolType {
-    TRIG, NUMBER, VARIABLE, LOG, SQRT, WHITESPACE, LPAREN, RPAREN,
-    PLUS, MINUS, TIMES, DIVIDE, CARROT,  FAIL
+    // iterate until loop breaks
+    for (;;) {
+      // default token is empty
+      Symbol current;
+      // we need to catch the exceptions
+      try {
+        current = flex.next_token();
+      } catch (IOException|RuntimeException e) {
+        return Optional.empty();
+      }
+
+      // the scanner returns 0 when finished
+      if (current.sym == 0) {
+        break;
+      }
+
+      tokens.add(current);
+    }
+
+    return Optional.of(tokens.stream().map(x -> sym.terminalNames[x.sym]).collect(Collectors.toList()));
   }
 }
--- src/test/java/horeilly1101/Parser/ScannerTest.java
+++ src/test/java/horeilly1101/Parser/ScannerTest.java
@@ -1,6 +1,10 @@
 package horeilly1101.Parser;
 
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.StringReader;
+
 import static horeilly1101.Parser.Scanner.*;
 import static horeilly1101.Parser.FlexScanner.*;
 
@@ -8,15 +12,25 @@ public class ScannerTest {
 
   @Test
   public void readTest() {
-//    System.out.println("hey");
-//
-//    String str = "12 + 34 * sin(1)";
-//    System.out.println(read(str));
-//
-//    String str2 = "12 + 34 * sin(1) / csc(x) + a";
-//    System.out.println(read(str2));
-//
-//    String str3 = "12 8 $";
-//    System.out.println(read(str3));
+    System.out.println("hey");
+
+    String str = "12 + 34 * sin(1)";
+//    FlexScanner scan = new FlexScanner(new StringReader(str));
+//    for(int i = 0; i < 10; i++) {
+//      try {
+//        System.out.println(scan.next_token().sym);
+//      } catch (RuntimeException| IOException e) {
+//        System.out.println(e);
+//      }
+//    }
+
+
+    System.out.println(read(str));
+
+    String str2 = "12 + 34 * sin(1) / csc(x) + a";
+    System.out.println(read(str2));
+
+    String str3 = "12 8 $";
+    System.out.println(read(str3));
   }
 }
