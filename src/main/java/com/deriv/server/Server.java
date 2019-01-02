4cd2f03f95e1816390c17002a22ab89be6cdf34e
Hugh O'Reilly <horeilly1101@gmail.com>
2019-01-02 00:42:16 -0600

    server is set up for derivatives

--- src/main/java/com/deriv/server/Server.java
+++ src/main/java/com/deriv/server/Server.java
@@ -1,10 +1,47 @@
 package com.deriv.server;
 
+import com.deriv.expression.Expression;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.Optional;
+
+import static com.deriv.parser.Parser.parse;
 import static spark.Spark.*;
 
 public class Server {
   // runs the server on localhost:4567
   public static void main(String[] args) {
-    get("/", (req, res) -> "Hello World");
+    get("/differentiate/:expr/:wrt", (req, res) -> {
+      // url variables
+      String expr = req.params(":expr");
+      String wrt = req.params(":wrt");
+
+      // evaluate derivative, if possible
+      Optional<Expression> oParsed = parse(expr);
+      String result = oParsed.isPresent()
+                          ? oParsed.get().differentiate(wrt).toString()
+                          : "ERROR";
+
+      return (new JSONObject())
+                 .put("expression", expr)
+                 .put("result", result);
+    });
+
+//    get("/evaluate/:expr/:at/:with", (req, res) -> {
+//      // url variables
+//      String expr = req.params(":expr");
+//      String at = req.params(":at");
+//      String with = req.params(":with");
+//
+//      // evaluate derivative, if possible
+//      Optional<Expression> oParsed = parse(expr);
+//      String result = oParsed.isPresent()
+//                          ? oParsed.get().evaluate(at, ).toString()
+//                          : "ERROR";
+//
+//      return (new JSONObject())
+//                 .put("expression", expr)
+//                 .put("result", result);
+//    });
   }
 }
---
+++ src/test/java/com/deriv/server/ServerTest.java
@@ -0,0 +1,18 @@
+package com.deriv.server;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ServerTest {
+  @Test
+  public void jsonTest() {
+    List<Integer> lst = Stream.of(1, 2, 3).collect(Collectors.toList());
+    JSONArray obj = (new JSONArray());
+    System.out.println(obj);
+  }
+}
