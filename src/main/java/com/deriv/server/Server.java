67beac0ef00b08af447bf66b8affe8fb3f077266
Hugh O'Reilly <horeilly1101@gmail.com>
2019-01-02 18:45:19 -0600

    added monadic error handling in Server.java

--- src/main/java/com/deriv/server/Server.java
+++ src/main/java/com/deriv/server/Server.java
@@ -1,7 +1,6 @@
 package com.deriv.server;
 
 import com.deriv.expression.Expression;
-import com.sun.org.apache.xml.internal.resolver.readers.ExtendedXMLCatalogReader;
 import org.json.JSONObject;
 import java.util.Optional;
 
@@ -19,29 +18,41 @@ public class Server {
 
   // runs the server on localhost:4567
   public static void main(String[] args) {
+    // the GET call that differentiates an expression
     get("/differentiate/:expr/:var", (req, res) -> {
       // url variables
-      String exprString = req.params(":expr");
-      String wrt = req.params(":var");
+      String expr = req.params(":expr");
+      String var = req.params(":var");
 
-      // evaluate derivative, if possible
-      Optional<Expression> oParsed = parse(exprString);
-      String expression = oParsed.isPresent()
-                              ? oParsed.get().toString()
-                              : "ERROR";
-      String result = oParsed.isPresent()
-                          ? oParsed.get().differentiate(wrt).toString()
-                          : "ERROR";
+      // attempt to parse var
+      Optional<String> oVar = var.length() == 1
+                                  ? parse(var).map(Expression::toString)
+                                  : Optional.empty();
 
-      return jobject()
-                 .put("data",
-                     jobject()
-                       .put("expression", expression)
-                       .put("wrt", wrt)
-                       .put("result", result)
-                 );
+      // attempt to parse expr
+      Optional<Expression> oExpr = parse(expr);
+
+      // attempt to differentiate expression
+      Optional<Expression> oDeriv = oVar.flatMap(vr ->
+                                                    oExpr.map(ex -> ex.differentiate(vr)));
+
+      // return the derivative if possible, otherwise an error
+      return oDeriv.isPresent()
+                 ? jobject()
+                       .put("data",
+                           jobject()
+                               .put("expression", oExpr.get().toString())
+                               .put("result", oDeriv.get().toString())
+                               .put("var", oVar.get())
+                       )
+                 : jobject()
+                       .put("error",
+                           jobject()
+                               .put("code", 400)
+                               .put("problem", "input(s) not valid"));
     });
 
+    // the GET call that evaluates an expression
     get("/evaluate/:expr/:var/:val", (req, res) -> {
       // url variables
       String expr = req.params(":expr");
@@ -64,10 +75,12 @@ public class Server {
       // attempt to parse expr
       Optional<Expression> oExpr = parse(expr);
 
+      // attempt to evaluate expression
       Optional<Expression> oEval = oVal.flatMap(vl ->
                                                     oVar.flatMap(vr ->
                                                                      oExpr.map(ex -> ex.evaluate(vr, vl))));
 
+      // return the evaluation if possible, otherwise an error
       return oEval.isPresent()
                  ? jobject()
                      .put("data",
