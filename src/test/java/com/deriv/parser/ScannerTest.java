bd294a0bd8592f2daf0dd322dc94341213ae18f6
Hugh O'Reilly <horeilly1101@gmail.com>
2019-01-04 16:04:03 -0600

    began adding tests to ScannerTest

--- src/test/java/com/deriv/parser/ScannerTest.java
+++ src/test/java/com/deriv/parser/ScannerTest.java
@@ -1,19 +1,85 @@
 package com.deriv.parser;
 
 import org.junit.jupiter.api.Test;
-import static com.deriv.parser.Parser.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class ScannerTest {
+import static com.deriv.parser.Parser.read;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ScannerTest {
+  /**
+   * This method tests (a) that the input can be parsed to an Expression
+   * and (b) that the parsed expression is equal to the expected expression.
+   */
+  private static void testHelper(List<Integer> expected, String input) {
+    Optional<List<Integer>> parsed = read(input);
+    assertTrue(parsed.isPresent());
+    assertEquals(expected, parsed.get());
+  }
+
+  /**
+   * This method allows to quickly, and functionally, construct a list
+   * of Integers.
+   */
+  private static List<Integer> listof(Integer... input) {
+    return Stream.of(input).collect(Collectors.toList());
+  }
 
   @Test
-  public void readTest() {
-    String str = "12 + 34 * sin(1)";
-    System.out.println(read(str));
+  void whiteSpaceTest() {
+    String str = "   ";
+    List<Integer> ex = listof();
+    testHelper(ex, str);
+  }
 
-    String str2 = "12 + 34 * sin(1) / csc(x) + a";
-    System.out.println(read(str2));
+  @Test
+  void addTest() {
+    String str = "5 + x";
+    List<Integer> ex = listof(sym.NUMBER, sym.PLUS, sym.VARIABLE);
+    testHelper(ex, str);
 
-    String str3 = "12 8 $";
-    System.out.println(read(str3));
+    String str2 = "5 + + x";
+    List<Integer> ex2 = listof(sym.NUMBER, sym.PLUS, sym.PLUS, sym.VARIABLE);
+    testHelper(ex2, str2);
+  }
+
+  @Test
+  void minusTest() {
+    String str = "5 - x";
+    List<Integer> ex = listof(sym.NUMBER, sym.MINUS, sym.VARIABLE);
+    testHelper(ex, str);
+
+    String str2 = "5 - - x";
+    List<Integer> ex2 = listof(sym.NUMBER, sym.MINUS, sym.MINUS, sym.VARIABLE);
+    testHelper(ex2, str2);
+
+    String str3 = "5 -- x";
+    testHelper(ex2, str3);
+  }
+
+  @Test
+  void multTest() {
+    String str = "5 * x";
+    List<Integer> ex = listof(sym.NUMBER, sym.TIMES, sym.VARIABLE);
+    testHelper(ex, str);
+
+    String str2 = "5 * * x";
+    List<Integer> ex2 = listof(sym.NUMBER, sym.TIMES, sym.TIMES, sym.VARIABLE);
+    testHelper(ex2, str2);
+  }
+
+  @Test
+  void divideTest() {
+    String str = "5 / x";
+    List<Integer> ex = listof(sym.NUMBER, sym.DIVIDE, sym.VARIABLE);
+    testHelper(ex, str);
+
+    String str2 = "5 / / x";
+    List<Integer> ex2 = listof(sym.NUMBER, sym.DIVIDE, sym.DIVIDE, sym.VARIABLE);
+    testHelper(ex2, str2);
   }
 }
