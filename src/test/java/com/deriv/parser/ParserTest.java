23f8f881720ba3a0e1e5329e116138e752276b3b
Hugh O'Reilly <horeilly1101@gmail.com>
2019-01-04 15:01:53 -0600

    added subtract tests in ParserTest

--- src/test/java/com/deriv/parser/ParserTest.java
+++ src/test/java/com/deriv/parser/ParserTest.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import static com.deriv.expression.Add.*;
 import static com.deriv.expression.Constant.*;
+import static com.deriv.expression.Log.ln;
 import static com.deriv.expression.Mult.*;
 import static com.deriv.expression.Variable.*;
 import static com.deriv.expression.Trig.*;
@@ -45,7 +46,25 @@ class ParserTest {
 
   @Test
   void subtractTest() {
+    String str = "sin(x) - 12";
+    Expression ex = add(sin(x()), constant(-12));
+    testHelper(ex, str);
 
+    String str2 = "1 - 12";
+    Expression ex2 = constant(-11);
+    testHelper(ex2, str2);
+
+    String str3 = "1 - 1 - 1 - 1 - 1";
+    Expression ex3 = constant(-3);
+    testHelper(ex3, str3);
+
+    String str4 = "5 - 0";
+    Expression ex4 = constant(5);
+    testHelper(ex4, str4);
+
+    String str5 = "5 - (ln(x) + sin(x))";
+    Expression ex5 = add(constant(5), negate(add(ln(x()), sin(x()))));
+    testHelper(ex5, str5);
   }
 
   @Test
