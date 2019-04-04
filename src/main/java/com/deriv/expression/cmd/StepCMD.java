c77944378ace6a9d127aee27000e64723b1bf996
Hugh O'Reilly <horeilly1101@gmail.com>
2019-04-03 21:34:04 -0500

    added new cmd

--- src/main/java/com/deriv/expression/cmd/StepCMD.java
+++ src/main/java/com/deriv/expression/cmd/StepCMD.java
@@ -1,4 +1,23 @@
-//package com.deriv.expression.cmd;
-//
-//public class StepCMD {
-//}
+package com.deriv.expression.cmd;
+
+import com.deriv.expression.AExpression.*;
+import com.deriv.expression.Expression;
+import com.deriv.util.Tuple;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StepCmd implements IStepCmd {
+  private List<Tuple<Step, Expression>> steps;
+
+  public StepCmd() {
+    this.steps = new ArrayList<>();
+  }
+
+  public void addStep(Step step, Expression expression) {
+    this.steps.add(Tuple.of(step, expression));
+  }
+
+  public List<Tuple<Step, Expression>> getSteps() {
+    return steps;
+  }
+}
