69f2d4b1a697ad6b6f80ad5f968f830168ac9be3
Hugh O'Reilly <horeilly1101@gmail.com>
2019-05-11 18:21:31 -0500

    trivial changes

--- src/main/java/com/deriv/expression/cmd/IStepCmd.java
+++ src/main/java/com/deriv/expression/cmd/IStepCmd.java
@@ -4,8 +4,21 @@ import com.deriv.expression.step.ExpressionWrapper;
 import com.deriv.util.ICmd;
 import com.deriv.util.Tree;
 
+/**
+ * Command that allows us to keep track of the steps taken to compute a given
+ * derivative.
+ */
 public interface IStepCmd extends ICmd {
+  /**
+   * Add a step to our storage.
+   * @param cmd input step
+   */
   void add(StepCmd cmd);
 
+  /**
+   * Getter method for the stored steps. Returns a tree, in an effort to maintain
+   * semantic clarity.
+   * @return steps
+   */
   Tree<ExpressionWrapper> getSteps();
 }
